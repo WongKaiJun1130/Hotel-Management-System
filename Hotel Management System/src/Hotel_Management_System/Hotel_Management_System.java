e3df4f3714df5e5cc16418aa75fb3e1a05cae238
Chia Kah Shun <156574261+chiakahshun@users.noreply.github.com>
2026-07-09 20:01:58 +0800

    Add files via upload

---
+++ Hotel Management System/src/Hotel_Management_System/Hotel_Management_System.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Hotel_Management_System;
+
+/**
+ *
+ * @author USER
+ */
+public class Hotel_Management_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mainUI menu = new mainUI();
+
+        menu.UI();
+        
+    }
+    
+}
