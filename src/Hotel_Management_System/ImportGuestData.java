6915078c652e181fc8b5b8b7093b6fe0712524bd
user <user@LAPTOP-735QM7MC>
2026-07-22 02:28:07 +0800

    change display format only

--- src/Hotel_Management_System/ImportGuestData.java
+++ src/Hotel_Management_System/ImportGuestData.java
@@ -25,8 +25,7 @@ public class ImportGuestData {
         guests.add(new Guest("R0009","Kevin Lim","Standard","Small Room","Waiting","28/07/2026"));
         guests.add(new Guest("R0010","Jessica Ng","Diamond","Middle Room","Waiting","29/07/2026"));
         GuestDatabase.saveGuests(guests);
-        System.out.println("10 Guests Imported Successfully!");
-
+        System.out.println( guests.getNumberOfEntries() + " Guests Saved!" );
     }
 
 }
