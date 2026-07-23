b15f6c35da2749fe8db13776f67700308163e06a
user <user@LAPTOP-735QM7MC>
2026-07-24 00:54:19 +0800

    when run can create the database

--- src/Hotel_Management_System/Hotel_Management_System.java
+++ src/Hotel_Management_System/Hotel_Management_System.java
@@ -6,11 +6,13 @@ package Hotel_Management_System;
 
 import System_UI.MainUI;
 import System_UI.VIPAllocationUI;
+import dao.GuestDatabase;
 
 public class Hotel_Management_System {
 
     public static void main(String[] args) {
-
+        GuestDatabase dao = new GuestDatabase();
+        dao.createGuestData();
         VIPAllocationUI.loadGuestDatabase();
         MainUI menu = new MainUI(); 
         menu.MainUI();
