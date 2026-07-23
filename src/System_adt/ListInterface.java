189979995b47a171b5e21352706d5fdd613ea38f
user <user@LAPTOP-735QM7MC>
2026-07-24 00:56:41 +0800

    change format

--- src/System_adt/ListInterface.java
+++ src/System_adt/ListInterface.java
@@ -8,7 +8,27 @@ package System_adt;
  *
  * @author USER
  */
-public interface ListInterface <T> {
+
+import java.util.Iterator;
+import java.util.Comparator;
+
+//================================================
+// List Interface for DoublyLinkedList
+//================================================
+    public interface ListInterface<T> {
+       public void insertAndAdvance(T data);
+       public T rollback();
+       public T redo();
+       public void spliceAfterCurrent(T data);
+       public T getCurrentData();
+       public boolean isEmpty();
+       public Iterator<T>getIterator();
+   }
+    
+//==============================================================================
+// Array List Interface 
+//==============================================================================
+  interface ArrayListInterface <T> {
     public boolean add(T newEntry);
     public boolean add(int newPosition,T newEntry);
     public T remove(int removePosition);
@@ -22,3 +42,31 @@ public interface ListInterface <T> {
     public void sort(java.util.Comparator<T> comparator);
     
 }
+//==============================================================================
+// Queue Interface 
+//==============================================================================
+ interface QueueInterface<T> {
+    public Iterator<T> getIterator();
+    public void enqueue(T newEntry);
+    public T dequeue();
+    public T getFront();
+    public boolean isEmpty();
+    public void clear();
+}
+
+
+//==============================================================================
+// Stack Interface 
+//==============================================================================
+ interface StackInterface<T> {
+    public void push(T newEntry);
+    public T pop();
+    public T peek();
+    public boolean isEmpty();
+    public void clear();
+    public int getCurrentSize();
+}
+
+
+
+    
