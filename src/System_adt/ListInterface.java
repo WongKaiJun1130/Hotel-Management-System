56aed355548d54d99244ac4fa3c127e219ef0b4c
winni <winni@LAPTOP-NN2V9AHL>
2026-07-25 15:36:31 +0800

    List Interface

--- src/System_adt/ListInterface.java
+++ src/System_adt/ListInterface.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package System_adt;
-
-/**
- *
- * @author USER
- */
 
 import java.util.Iterator;
 import java.util.Comparator;
@@ -15,62 +6,58 @@ import java.util.Comparator;
 //================================================
 // List Interface for DoublyLinkedList
 //================================================
-    public interface ListInterface<T> {
-       public void insertAndAdvance(T data);
-       public T rollback();
-       public T redo();
-       public void spliceAfterCurrent(T data);
-       public T getCurrentData();
-       public boolean isEmpty();
-       public Iterator<T>getIterator();
-   }
-    
+public interface ListInterface<T>{
+    void insertAndAdvance(T data);
+    T rollback();
+    T redo();
+    void spliceAfterCurrent(T data);
+    T getCurrentData();
+    boolean isEmpty();
+    Iterator<T> getIterator();
+}
+
 //==============================================================================
 // Array List Interface 
 //==============================================================================
-  interface ArrayListInterface <T> {
-    public boolean add(T newEntry);
-    public boolean add(int newPosition,T newEntry);
-    public T remove(int removePosition);
-    public void clear();
-    public boolean replace(int replacePosition,T newEntry);
-    public T getEntry(int givenPosition);
-    public int getNumberOfEntries();
-    public boolean contains(T anEntry);
-    public boolean isEmpty();
-    public boolean isFull();
-    public void sort(java.util.Comparator<T> comparator);
-    public int indexOf(T element);
-    public T getLast();
-    public int getSize();
-    public boolean remove(T element);
-
-    
-}
-//==============================================================================
-// Queue Interface 
-//==============================================================================
- interface QueueInterface<T> {
-    public Iterator<T> getIterator();
-    public void enqueue(T newEntry);
-    public T dequeue();
-    public T getFront();
-    public boolean isEmpty();
-    public void clear();
+interface ArrayListInterface<T>{
+    boolean add(T newEntry);
+    boolean add(int newPosition,T newEntry);
+    T remove(int removePosition);
+    void clear();
+    boolean replace(int replacePosition,T newEntry);
+    T getEntry(int givenPosition);
+    int getNumberOfEntries();
+    boolean contains(T anEntry);
+    boolean isEmpty();
+    boolean isFull();
+    int indexOf(T element);
+    T getLast();
+    int getSize();
+    boolean remove(T element);
 }
 
 
 //==============================================================================
 // Stack Interface 
 //==============================================================================
- interface StackInterface<T> {
-    public void push(T newEntry);
-    public T pop();
-    public T peek();
-    public boolean isEmpty();
-    public void clear();
-    public int getCurrentSize();
+interface StackInterface<T>{
+    void push(T newEntry);
+    T pop();
+    T peek();
+    boolean isEmpty();
+    void clear();
+    int getCurrentSize();
 }
 
 
-    
+//==============================================================================
+// Queue Interface 
+//==============================================================================
+interface QueueInterface<T>{
+    Iterator<T> getIterator();
+    void enqueue(T newEntry);
+    T dequeue();
+    T getFront();
+    boolean isEmpty();
+    void clear();
+}
