8748be39da4cc40920c3592c08dd7c47824a1387
Chia Kah Shun <156574261+chiakahshun@users.noreply.github.com>
2026-07-28 13:01:13 +0800

    wobuzhidaozaizuoshenme

--- src/dao/GuestDatabase.java
+++ src/dao/GuestDatabase.java
@@ -11,16 +11,16 @@ public class GuestDatabase {
     public static void createGuestData() {
         DoublyLinkedList.ArrayList<Guest> guests = new DoublyLinkedList.ArrayList<>();
 
-        guests.add(new Guest("R0001","John Tan","Elite","Big Room","Waiting","20/07/2026"));
-        guests.add(new Guest("R0002","Wong Lee","Diamond","Middle Room","Waiting","21/07/2026"));
-        guests.add(new Guest("R0003","Alice Lim","Platinum","Small Room","Waiting","22/07/2026"));
-        guests.add(new Guest("R0004","David Wong","Elite","Big Room","Waiting","23/07/2026"));
-        guests.add(new Guest("R0005","Jason Lee","Standard","Small Room","Waiting","24/07/2026"));
-        guests.add(new Guest("R0006","Sarah Tan","Diamond","Middle Room","Waiting","25/07/2026"));
-        guests.add(new Guest("R0007","Michael Chen","Platinum","Middle Room","Waiting","26/07/2026"));
-        guests.add(new Guest("R0008","Emily Wong","Elite","Big Room","Waiting","27/07/2026"));
-        guests.add(new Guest("R0009","Kevin Lim","Standard","Small Room","Waiting","28/07/2026"));
-        guests.add(new Guest("R0010","Jessica Ng","Diamond","Middle Room","Waiting","29/07/2026"));
+        guests.add(new Guest("R0001","John Tan","0123456789","Elite","Big Room","Waiting","20/07/2026"));
+        guests.add(new Guest("R0002","Wong Lee","0134567890","Diamond","Middle Room","Waiting","21/07/2026"));
+        guests.add(new Guest("R0003","Alice Lim","0145678901","Platinum","Small Room","Waiting","22/07/2026"));
+        guests.add(new Guest("R0004","David Wong","0166789012","Elite","Big Room","Waiting","23/07/2026"));
+        guests.add(new Guest("R0005","Jason Lee","0177890123","Standard","Small Room","Waiting","24/07/2026"));
+        guests.add(new Guest("R0006","Sarah Tan","0188901234","Diamond","Middle Room","Waiting","25/07/2026"));
+        guests.add(new Guest("R0007","Michael Chen","0199012345","Platinum","Middle Room","Waiting","26/07/2026"));
+        guests.add(new Guest("R0008","Emily Wong","0121122334","Elite","Big Room","Waiting","27/07/2026"));
+        guests.add(new Guest("R0009","Kevin Lim","0132233445","Standard","Small Room","Waiting","28/07/2026"));
+        guests.add(new Guest("R0010","Jessica Ng","0143344556","Diamond","Middle Room","Waiting","29/07/2026"));
         GuestDatabase dao = new GuestDatabase();
         dao.saveToFile(guests);
         
