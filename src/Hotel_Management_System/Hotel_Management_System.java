0b69c2b96b74760bd3c68177dcd361f67de734d5
winni <winni@LAPTOP-NN2V9AHL>
2026-08-02 20:18:21 +0800

    bookingui

--- src/Hotel_Management_System/Hotel_Management_System.java
+++ src/Hotel_Management_System/Hotel_Management_System.java
@@ -4,20 +4,23 @@
  */
 package Hotel_Management_System;
 
-import System_UI.MainUI;
+import System_UI.*;
 import dao.GuestDatabase;
 import dao.LoyaltyDatabase;
 import dao.RoomDao;
+
 
 public class Hotel_Management_System {
 
     public static void main(String[] args) {
         GuestDatabase guestDatabase = new GuestDatabase();
+        BookingUI bookingUI = new BookingUI();
         LoyaltyDatabase loyaltyDatabase = new LoyaltyDatabase();
         guestDatabase.createGuestData();
+        bookingUI.bookingMenu();
         loyaltyDatabase.createLoyaltyData();
         RoomDao.createRoomData();
         MainUI.MainUI();
-
+       
     }
 }
